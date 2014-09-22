f15a688655e8334a375d64dca39b25cd90b98f4c
AlwinEsch <alwin.esch@web.de>
2014-09-22 22:20:42 +0200

    ADSP - (0004) Allow usage of CAEChannelInfo CActiveAEResample::GetAEChannelLayout(uint64_t layout)

--- xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAEResample.h
+++ xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAEResample.h
@@ -45,7 +45,7 @@ public:
   int GetSrcBufferSize(int samples);
   int GetDstBufferSize(int samples);
   static uint64_t GetAVChannelLayout(CAEChannelInfo &info);
-//  static CAEChannelInfo GetAEChannelLayout(uint64_t layout);
+  static CAEChannelInfo GetAEChannelLayout(uint64_t layout);
   static AVSampleFormat GetAVSampleFormat(AEDataFormat format);
   static uint64_t GetAVChannel(enum AEChannel aechannel);
   int GetAVChannelIndex(enum AEChannel aechannel, uint64_t layout);
